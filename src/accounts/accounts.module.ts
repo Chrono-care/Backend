5132b86aa706314e7fe5e5c0eb31c2fa2718745e
jimmy foulard <jimmy.foulard@epitech.eu>
2025-02-06 15:20:23 +0100

    adding mail

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -3,7 +3,7 @@ import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from './entities/account.entity';
-import { MailsService } from '../mails/mails.service';
+import { MailsService } from 'src/mails/mails.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Account])],
