e352640a064ddcf062ec173a88869169b9155167
Panteleimonow <nicolas.panteleimonow@gmail.com>
2025-02-07 09:44:45 +0100

    fix: cors issue

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://punio.eu',
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  });
   await app.listen(4000);
 }
 bootstrap();
