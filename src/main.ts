e2fe60b88dd467189f7e0888d2bcc72ca7e8d43d
jimmy foulard <jimmy.foulard@epitech.eu>
2025-02-21 11:16:28 +0100

    removing yarn, deactivating sync database with Backend

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+const PORT = 8080;
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
